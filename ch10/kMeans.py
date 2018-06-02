189ea8a18bd3f3298d727c98b25ee8757f938428
Garrick <yejiayii@outlook.com>
2018-06-02 14:09:06 +0800

    cluster

--- ch10/kMeans.py
+++ ch10/kMeans.py
@@ -46,7 +46,7 @@ def kMeans(dataSet, k, distMeas=distEclud, createCent=randCent):
     while clusterChanged:# 直到没有点的簇更新了就退出循环
         clusterChanged = False
         for i in range(m):
-            minDist = np.inf;
+            minDist = np.inf
             minIndex = -1
             for j in range(k):# 寻找与其距离最近的质心
                 distJI = distMeas(centroids[j, :], dataSet[i, :])
@@ -62,3 +62,72 @@ def kMeans(dataSet, k, distMeas=distEclud, createCent=randCent):
             ptsInClust = dataSet[np.nonzero(clusterAssment[:, 0].A == cent)[0]]#get all the point in this cluster
             centroids[cent, :] = np.mean(ptsInClust, axis=0) #assign centroid to mean
     return centroids, clusterAssment
+
+
+def biKmeans(dataSet, k, distMeas=distEclud):
+    m = np.shape(dataSet)[0]
+    clusterAssment = np.mat(np.zeros((m, 2)))
+    # 整个数据集当成一个簇，计算其质心
+    centroid0 = np.mean(dataSet, axis=0).tolist()[0]
+    # 创建只有一个质心的列表
+    centList = [centroid0]
+    for j in range(m):#calc initial Error
+        clusterAssment[j, 1] = distMeas(np.mat(centroid0), dataSet[j, :])**2
+        while (len(centList) < k):
+            # SSE (Sum of Squared Error,误差平方和）
+            lowestSSE = np.inf
+            for i in range(len(centList)):
+                # 取得在当前簇中的所有数据点
+                ptsInCurrCluster = dataSet[np.nonzero(clusterAssment[:, 0].A == i)[0], :]
+                centroidMat, splitClustAss = kMeans(ptsInCurrCluster, 2, distMeas)
+                sseSplit = sum(splitClustAss[:, 1])#compare the SSE to the currrent minimum
+                sseNotSplit = sum(clusterAssment[np.nonzero(clusterAssment[:, 0].A != i)[0], 1])
+                print("sseSplit, and notSplit: ", sseSplit, sseNotSplit)
+                if (sseSplit + sseNotSplit) < lowestSSE:
+                    bestCentToSplit = i
+                    bestNewCents = centroidMat
+                    bestClustAss = splitClustAss.copy()
+                    lowestSSE = sseSplit + sseNotSplit
+            # 上面应用簇为2的分类得到的index有0和1，要改成新的
+            bestClustAss[np.nonzero(bestClustAss[:, 0].A == 1)[0], 0] = len(centList)
+            bestClustAss[np.nonzero(bestClustAss[:, 0].A == 0)[0], 0] = bestCentToSplit
+            print('the bestCentToSplit is: ', bestCentToSplit)
+            print('the len of bestClustAss is: ', len(bestClustAss))
+            centList[bestCentToSplit] = bestNewCents[0, :].tolist()[0]
+            centList.append(bestNewCents[1, :].tolist()[0])
+            # 赋予新的误差
+            clusterAssment[np.nonzero(clusterAssment[:, 0].A == bestCentToSplit)[0], :] = bestClustAss
+    return np.mat(centList), clusterAssment
+
+
+# import urllib
+# import json
+# def geoGrab(stAddress, city):
+#     apiStem = 'http://where.yahooapis.com/geocode?'  #create a dict and constants for the goecoder
+#     params = {}
+#     params['flags'] = 'J'#JSON return type
+#     params['appid'] = 'aaa0VN6k'
+#     params['location'] = '%s %s' % (stAddress, city)
+#     url_params = urllib.parse.urlencode(params)
+#     yahooApi = apiStem + url_params      #print url_params
+#     print(yahooApi)
+#     c = urllib.request.urlopen(yahooApi)
+#     return json.loads(c.read())
+#
+#
+# from time import sleep
+# def massPlaceFind(fileName):
+#     fw = open('places.txt', 'w')
+#     for line in open(fileName).readlines():
+#         line = line.strip()
+#         lineArr = line.split('\t')
+#         retDict = geoGrab(lineArr[1], lineArr[2])
+#         if retDict['ResultSet']['Error'] == 0:
+#             lat = float(retDict['ResultSet']['Results'][0]['latitude'])
+#             lng = float(retDict['ResultSet']['Results'][0]['longitude'])
+#             print("%s\t%f\t%f" % (lineArr[0], lat, lng))
+#             fw.write('%s\t%f\t%f\n' % (line, lat, lng))
+#         else:
+#             print("error fetching")
+#         sleep(1)
+#     fw.close()
--- ch10/test10.py
+++ ch10/test10.py
@@ -1,15 +1,19 @@
 # -*- coding: utf-8 -*-
 from ch10 import kMeans
 import numpy as np
+from importlib import reload
 
-
-datMat = np.mat(kMeans.loadDataSet('testSet.txt'))
+# datMat = np.mat(kMeans.loadDataSet('testSet.txt'))
 
 # print(np.min(datMat[:, 0]))
 # print(np.max(datMat[:, 1]))
 # print(kMeans.randCent(datMat, 2))
 # print(kMeans.distEclud(datMat[0], datMat[1]))
 
-myCentroids, clustAssing = kMeans.kMeans(datMat, 4)
-
-print(myCentroids)
+# myCentroids, clustAssing = kMeans.kMeans(datMat, 4)
+# print(myCentroids)
+# reload(kMeans)
+# datMat3 = np.mat(kMeans.loadDataSet('testSet2.txt'))
+# centList, myNewAssments = kMeans.biKmeans(datMat3, 3)
+# print(centList)
+geoResults = kMeans.geoGrab('1 VA Center', 'Augusta, ME')
